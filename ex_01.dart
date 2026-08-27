ff0bb9433b146ab3285e5be8063f1297925e7d2b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-26 22:27:03 -0300

    commit 2

---
+++ ex_01.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nome = 'Joao Vitor';
+  int idade = 26;
+  double altura = 1.68;
+  bool status = true;
+
+  print('Nome: $nome');
+  print('Idade: $idade');
+  print('Altura: $altura');
+  print('Estudante: $status');
+}
